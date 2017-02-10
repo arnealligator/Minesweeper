a04fb765e5d1f0e40d1554ddca2f30b0452bc040
arnealligator <johnedvinsvedberg@gmail.com>
2017-02-11 00:04:44 +0100

    Add files via upload

---
+++ Minesweeper/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ProjektProgram
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+
+        public static Form1 Form;
+
+        static void Main()
+        {
+            Form = new Form1();
+            Application.Run(Form);
+        }
+
+    }
+
+
+}
